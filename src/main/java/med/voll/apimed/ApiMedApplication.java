463b24c523138c1731e23208963581d97dc35e49
Daniel Imperato <80261360+Imperat0@users.noreply.github.com>
2023-06-29 22:04:17 -0300

    Initial commit

---
+++ src/main/java/med/voll/apimed/ApiMedApplication.java
@@ -0,0 +1,13 @@
+package med.voll.apimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/apimed/ApiMedApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.apimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
